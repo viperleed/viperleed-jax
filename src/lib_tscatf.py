038cd31d9f6148a30f5077d3cf8dc3f4d18cc2d1
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-01-24 10:52:40 +0100

    minor: change message

--- src/lib_tscatf.py
+++ src/lib_tscatf.py
@@ -202,7 +202,7 @@ def MATEL_DWG(NCSTEP,AF,NewAF,E,VV,VPI,LMAX,n_beams,EXLM,ALM,AK2M,
                     CAK = 2*E-2j*VPI+0.0000001j
                     CAK = np.sqrt(CAK)
                     if D >= 2*E:
-                        print('irgendwas ist schiefgegangen in MATEL_DWG')
+                        print('Something went wrong in MATEL_DWG') # @Paul: can we remove this?
                         return 0
 #                   XA is evaluated relative to the muffin tin zero i.e. it uses energy= incident electron energy +
 #                   inner potential
