2539945c70d531745bc32bc09eec8a0658015b95
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-02-19 17:32:15 +0100

    feat: natural knots
    currently not used in favour of not-a-knot boundary conditions

--- src/interpolation.py
+++ src/interpolation.py
@@ -14,3 +14,31 @@ def find_interval(knots, x_val):
         raise ValueError('knots must be sorted')
     return jnp.searchsorted(knots, x_val, side='left') -1
 
+## Natural knots boundary condition – currently unused – TODO: implement?
+def get_natural_knots(x, deg):
+    knots = np.concatenate(
+        [np.full(shape=(deg,), fill_value=x[0]),
+         x,
+         np.full(shape=(deg,), fill_value=x[-1])])
+    return knots
+
+def natural_derivative_bc(deg):
+    _supported_degrees = (3, 5)
+
+    if deg == 3:
+        derivs_l_ord = np.array([2])
+        derivs_l_val = np.array([0])
+        
+        derivs_r_ord = np.array([2])
+        derivs_r_val = np.array([0])
+
+    elif deg == 5:
+        derivs_l_ord = np.array([3, 4])
+        derivs_l_val = np.array([0, 0])
+        
+        derivs_r_ord = np.array([3, 4])
+        derivs_r_val = np.array([0, 0])
+
+    else:
+        raise ValueError(f"unsupported degree {deg}")
+    return derivs_l_ord, derivs_l_val, derivs_r_ord, derivs_r_val
