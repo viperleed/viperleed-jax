16c736e9734f5410e3e3f018a3be2a7a8badec33
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-02-19 17:31:46 +0100

    feat: find_interval function

--- src/interpolation.py
+++ src/interpolation.py
@@ -7,3 +7,10 @@ import jax
 from jax import numpy as jnp
 from scipy import interpolate
 
+def find_interval(knots, x_val):
+    """Return index of interval in knots that contains x_val"""
+    # raise if knots are not sorted
+    if not jnp.all(knots[:-1] <= knots[1:]):
+        raise ValueError('knots must be sorted')
+    return jnp.searchsorted(knots, x_val, side='left') -1
+
