fix: numerical stability for autodiff in harmony
The arccos and arctan functions used in the harmony subroutine need special precautions to avoid NaNs/divergence around x=0 and for their gradients.

src/lib_math.py
@@ -11,6 +11,30 @@ from spherical_bessel import functions
 # numerical epsilon to avoid division by zero
 EPS = 1e-8
 
+
+def _divide_zero_safe(
+    numerator: jnp.ndarray,
+    denominator: jnp.ndarray,
+    limit_value: float = 0.0,
+) -> jnp.ndarray:
+    """Function that forces the result of dividing by 0 to be equal to a limit
+    value in a jit- and autodiff-compatible way
+
+    Args:
+        numerator: Values in the numerator
+        denominator: Values in the denominator, may contain zeros
+        limit_value: Value to return where denominator == 0.0
+    Returns:
+        numerator / denominator with result == 0.0 where denominator == 0.0
+    """
+    denominator_masked = jnp.where(denominator == 0.0, 1.0, denominator)
+    return jnp.where(
+        denominator == 0.0,
+        limit_value,
+        numerator / denominator_masked,
+    )
+
+
 def _generate_bessel_functions(l_max):
     """Generate a list of spherical Bessel functions up to order l_max"""
     bessel_functions = []
@@ -51,7 +75,10 @@ def HARMONY(C, LMAX, dense_l, dense_m):
     This is a python implementation of the fortran subroutine HARMONY from
     TensErLEED. It uses the jax.scipy.special.sph_harm function to produce
     equivalent results."""
-    r = jnp.sqrt(C[0] ** 2 + C[1] ** 2 + C[2] ** 2 + EPS)
-    theta = jnp.arccos(C[0] / r)
-    phi = jnp.arctan2(C[2], C[1])
-    return sph_harm(dense_m, dense_l, jnp.array([phi]), jnp.array([theta]), n_max=LMAX)
+    r = jnp.linalg.norm(C)
+    eps_sign_z = EPS*jnp.sign(C[0])
+    theta = jnp.arccos(_divide_zero_safe(C[0], jnp.linalg.norm(C), 1.0)-eps_sign_z)
+    # Alternative implementation to avoid division by zero:
+    # theta = jnp.arccos((C[0]+eps_sign_z)/(jnp.linalg.norm(C)+EPS)-eps_sign_z)
+    phi = jnp.arctan2(C[2]+EPS, C[1]+EPS)
+    return sph_harm(dense_m, dense_l, jnp.asarray([phi]), jnp.asarray([theta]), n_max=LMAX)