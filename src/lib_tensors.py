c0d5aecc2384fec282591f5a9baff7aa855c001d
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-02-28 19:18:59 +0100

    feat:tensor dataclass

--- src/lib_tensors.py
+++ src/lib_tensors.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import numpy as np
 import fortranformat as ff
 
@@ -7,6 +9,40 @@ FF_READER_4E16_12 = ff.FortranRecordReader("4E16.12")
 FF_READER_5E12_6 = ff.FortranRecordReader("5E12.6")
 FF_READER_4E12_6 = ff.FortranRecordReader("4E12.6")
 FF_READER_I5 = ff.FortranRecordReader("I5")
+
+
+# TODO: implement consistency checks for the tensor files
+@dataclass
+class TensorData:
+    """Holds the data read from a tensor file
+
+    Tensor files are atom specific and generated in the reference-calculation
+    for every non-bulk atom. They contain the atomic t-matrices and the
+    scattering amplitudes for the reference structure.
+    While the t-matrices are unique, every tensor file contains the same
+    reference-amplitudes."""
+    # attribute name                      TensErLEEED variable
+    e_kin: np.ndarray                     # E
+    v0i_substrate: np.ndarray             # VPIS
+    v0i_overlayer: np.ndarray             # VPIO
+    v0r: np.ndarray                       # VV
+    n_phaseshifts_per_energy: np.ndarray  # L1DAT
+    ref_amps: np.ndarray                  # XIST
+    t_matrix: np.ndarray                  # CAF
+    tensor_amps_in: np.ndarray            # ALM
+    tensor_amps_out: np.ndarray           # EXLM
+    k_delta: np.ndarray                   # PSQ
+    kx_in: np.ndarray                     # AK2M
+    ky_in: np.ndarray                     # AK3M
+
+    @property
+    def n_energies(self):
+        return self.e_kin.size
+
+    @property
+    def n_beams(self):           # NTO
+        return self.ref_amps.shape[1]
+
 
 def read_tensor(filename, n_beams=9, n_energies=100, l_max = 11):
     # Reading in the data of a file
@@ -25,7 +61,7 @@ def read_tensor(filename, n_beams=9, n_energies=100, l_max = 11):
     v0r = np.full((n_energies,), dtype=np.float64, fill_value=np.nan)
     n_phaseshifts_per_energy = np.full(
         (n_energies,), dtype=np.int_, fill_value=0)
-    
+
     # t_matrix indexed by: energy, l
     t_matrix = np.full((n_energies, l_max) ,dtype=np.complex128, fill_value = 0)
     ref_amps = np.full((n_energies, n_beams),
@@ -33,7 +69,7 @@ def read_tensor(filename, n_beams=9, n_energies=100, l_max = 11):
     # may want to rethink how to index this array. Would it be better to do it the natural way, ie T[l,m,l',m']?
     tensor_amps_in = np.full((n_energies, (l_max+1)**2),
                              dtype=np.complex128, fill_value=0.0)
-    tensor_amps_exit = np.full((n_energies, n_beams,(l_max+1)**2),
+    tensor_amps_out = np.full((n_energies, n_beams,(l_max+1)**2),
                           dtype=np.complex128, fill_value=0.0)
 
     delta_kx = np.full((n_energies, n_beams) ,dtype=np.float64, fill_value = 0)
@@ -79,14 +115,11 @@ def read_tensor(filename, n_beams=9, n_energies=100, l_max = 11):
 
         # blocks for exiting beams
         while True:
-            
             # number of beam
             line = next(file_lines)
             n_beam = FF_READER_I5.read(line)[0]
-            
             if n_beam< 0:
                 break
-            
             # delta k and k
             line = next(file_lines)
             delta_kx[e_step, n_beam - 1], delta_ky[e_step, n_beam - 1], kx_in[e_step, n_beam - 1], ky_in[e_step, n_beam - 1] = FF_READER_4E12_6.read(
@@ -96,33 +129,28 @@ def read_tensor(filename, n_beams=9, n_energies=100, l_max = 11):
             tens_amps_as_real = read_block(FF_READER_5E12_6, file_lines, shape=(
                 n_phaseshifts_per_energy[e_step]**2, 2))
             tens_amps_as_complex = tens_amps_as_real.view(np.complex128)[..., 0]
-            tensor_amps_exit[e_step, n_beam-1, :n_phaseshifts_per_energy[e_step]**2] = tens_amps_as_complex
-
+            tensor_amps_out[e_step, n_beam-1, :n_phaseshifts_per_energy[e_step]**2] = tens_amps_as_complex
 
     k_delta = np.full((n_energies, 2, n_beams) ,dtype=np.float64, fill_value = np.nan)
     k_delta[:, 0, :] = delta_kx
     k_delta[:, 1, :] = delta_ky
 
-    tensor_amps_exit = np.transpose(tensor_amps_exit, axes=(0, 2, 1))
+    tensor_amps_out = np.transpose(tensor_amps_out, axes=(0, 2, 1))
 
-    tensor_data_dict = {
-        "e_kin": e_kin, #E
-        "v0i_substrate": v0i_substrate, #VPIS
-        "v0i_overlayer": v0i_overlayer, #VPIO
-        "v0r": v0r, #VV
-        "n_phaseshifts_per_energy": n_phaseshifts_per_energy, #L1DAT
-        "t_matrix": t_matrix, #CAF
-        "ref_amps": ref_amps, #XIST
-        "tensor_amps_in": tensor_amps_in, #ALM
-        "tensor_amps_out": tensor_amps_exit, #EXLM
-        "kx_in": kx_in, #AK2M
-        "ky_in": ky_in, #AK3M
-        "k_delta": k_delta, #PSQ
-        "n_energies": n_energies,
-        "n_beams": n_beams, #NT0
-        }
-
-    return tensor_data_dict
+    return TensorData(
+        e_kin=e_kin,
+        v0i_overlayer=v0i_overlayer,
+        v0i_substrate=v0i_substrate,
+        v0r=v0r,
+        ref_amps=ref_amps,
+        t_matrix=t_matrix,
+        n_phaseshifts_per_energy=n_phaseshifts_per_energy,
+        tensor_amps_in=tensor_amps_in,
+        tensor_amps_out=tensor_amps_out,
+        kx_in=kx_in,
+        ky_in=ky_in,
+        k_delta=k_delta
+        )
 
 
 def read_block(reader, lines, shape, dtype=np.float64): 
