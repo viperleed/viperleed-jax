92a8ba4d39e977c680ca383f94186a7c162d7a06
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-03-13 16:03:16 +0100

    feat: reorder axis in delta_amplitude calculation
    for better memory access and scaling

--- src/delta.py
+++ src/delta.py
@@ -18,17 +18,25 @@ def delta_amplitude(LMAX, DR, energies, tensors, unit_cell_area, phaseshifts, di
     _phaseshifts = phaseshifts.val
     # unpack tensor data
     t_matrix_ref = jnp.array([t.t_matrix for t in tensors])
+    t_matrix_ref = t_matrix_ref.swapaxes(0, 1)  # swap energy and atom indices
     v_imag = tensors[0].v0i_substrate[0]
+
+    # atom dependent quantities
     tensor_amps_out = jnp.array([t.tensor_amps_out for t in tensors])
     tensor_amps_in = jnp.array([t.tensor_amps_in for t in tensors])
+    tensor_amps_out = tensor_amps_out.swapaxes(0, 1)  # swap energy and beam indices
+    tensor_amps_in = tensor_amps_in.swapaxes(0, 1)  # swap energy and beam indices
+
+    # energy dependent quantities
     out_k_par2 = tensors[0].kx_in # same for all atoms
     out_k_par3 = tensors[0].ky_in # same for all atoms
 
     tscatf_vmap = jax.vmap(apply_vibrational_displacements, in_axes=(None, 0, 0, None), out_axes=1)  # vmap over energy
     t_matrix_new = tscatf_vmap(LMAX, _phaseshifts, _energies, DR)
+    t_matrix_new = t_matrix_new.swapaxes(0, 1)  # swap energy and atom indices
 
     # amplitude differences
-    matel_dwg_vmap_energy = jax.vmap(apply_geometric_displacements, in_axes=(1, 1, 0, None, None, 1, 1, 0, 0, None, None))
+    matel_dwg_vmap_energy = jax.vmap(apply_geometric_displacements, in_axes=(0, 0, 0, None, None, 0, 0, 0, 0, None, None))
     d_amplitude = matel_dwg_vmap_energy(t_matrix_ref, t_matrix_new, _energies, v_imag,
                         LMAX, tensor_amps_out, tensor_amps_in, out_k_par2, out_k_par3,
                         unit_cell_area, displacements)
