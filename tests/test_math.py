b4416d0ba1566bb01944edfdbe4b7922aae58b35
Paul <haideggerpaul@gmail.com>
2024-04-02 16:11:24 +0200

    tests for HARMONY

--- tests/test_math.py
+++ tests/test_math.py
@@ -3,6 +3,7 @@ import pytest
 
 from jax import numpy as jnp
 from src.lib_math import _divide_zero_safe
+from src.lib_math import *
 
 
 class TestDivideZeroSafe:
@@ -53,3 +54,26 @@ class TestDivideZeroSafe:
         result = _divide_zero_safe(numerator, denominator, limit_value=-999.0)
         expected_result = jnp.array([0.5, 0.5, 0.6])
         jnp.allclose(result, expected_result)
+
+class TestHARMONY:
+    # Testcase for basic functionality by comparing it with sph_harm
+    def test_HARMONY_normal_case(self):
+        # Define input vector C for normal case
+        C = jnp.array([1.0, 2.0, 3.0])
+        LMAX = 2
+        # Compute expected output using sph_harm directly
+        expected_output = sph_harm(DENSE_M[2*LMAX], DENSE_L[2*LMAX], jnp.array([np.arctan2(3.0+EPS, 2.0+EPS)]), jnp.array([np.arccos((1.0+EPS)/safe_norm(C))]), n_max=LMAX)
+        # Compare with the output of HARMONY function
+        assert jnp.allclose(HARMONY(C, LMAX), expected_output)
+    
+    def test_HARMONY_division_by_zero(self):
+        # Define input vector C where division by zero might occur
+        C = jnp.array([1.0, 0.0, 0.0])
+        LMAX = 2
+        # Compute expected output manually as sph_harm will raise errors
+        expected_output = sph_harm(DENSE_M[2*LMAX], DENSE_L[2*LMAX], np.pi*0.25, 0, n_max=LMAX)
+        # Compare with the output of HARMONY function
+        assert jnp.allclose(HARMONY(C, LMAX), expected_output, atol=1e-03) #very big because arccos is sensible to small changes
+
+if __name__ == "__main__":
+    pytest.main([__file__])
