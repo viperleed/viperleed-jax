35c4a9bbc3d922d37e054ac5f2d4aec8cbb025ab
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-05-16 16:21:50 +0200

    fix: missing line from previous commit

--- src/tensor_calculator.py
+++ src/tensor_calculator.py
@@ -135,6 +135,7 @@ class TensorLEEDCalculator:
     def R_pendry_val_and_grad(self, vib_amps, displacements, v0_real_steps):
         # TODO: urgent: currently only gives gradients for geo displacements
         return jax.value_and_grad(self._R_pendry, argnums=(1))(vib_amps, displacements, v0_real)
+        return jax.value_and_grad(self._R_pendry, argnums=(1))(vib_amps, displacements, v0_real_steps)
 
     @property
     def zero_displacement():
