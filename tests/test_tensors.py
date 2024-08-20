fe1b1807ef48a68a248ea995de7ece831a6188f1
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-08-20 16:37:42 +0200

    test,feat: test tensors

--- tests/test_tensors.py
+++ tests/test_tensors.py
@@ -1,10 +1,52 @@
+import pickle
+
 import pytest
 import numpy as np
 
-from src.files.tensors import read_tensor
+from src.files.tensors import read_tensor_zip
 
-# TODO
-#read_tensor_num = lambda num: read_tensor(data_path / 'Tensors' / f'T_{num}', n_beams=len(rparams.ivbeams), n_energies=ref_energies.size, l_max=LMAX+1)
-#tensors = [read_tensor_num(at.num) for at in non_bulk_atoms]
+# Reading the zipped tensors takes 5+ minutes
+@pytest.fixture(scope='session')
+def fe2o3_read_tensor_zip(fe2o3_unrelaxed_tensor_path):
+    tensors = read_tensor_zip(fe2o3_unrelaxed_tensor_path, lmax=14, n_beams=38, n_energies=208)
+    return tensors
 
-# TODO: check consistency of known tensors
+# Therefore, we have pickeled tensors for faster testing
+@pytest.fixture(scope='session')
+def fe2o3_pickled_tensor(fe2o3_unrelaxed_pickle_path):
+    with open(fe2o3_unrelaxed_pickle_path, 'rb') as f:
+        tensors = pickle.load(f)
+    return tensors
+
+# We can test that the read tensors are consistent with the pickled tensors
+# and then do the other tests (also in the other files) from the pickled version
+
+class TestReadTensors:
+    # Test read_tensor_zip and check consistency vs. pickled tensors
+
+    def test_read_tensor_length(self, fe2o3_read_tensor_zip, fe2o3_pickled_tensor):
+        assert len(fe2o3_read_tensor_zip) == 30
+        assert len(fe2o3_read_tensor_zip) == len(fe2o3_pickled_tensor)
+
+    def test_read_tensor_consistency(self, fe2o3_read_tensor_zip, fe2o3_pickled_tensor):
+        for file_name in fe2o3_read_tensor_zip.keys():
+            assert file_name in fe2o3_pickled_tensor.keys()
+            assert fe2o3_read_tensor_zip[file_name].is_consistent(fe2o3_pickled_tensor[file_name])
+
+
+class TestPickledTensors:
+    def test_tensors_self_consistent(self, fe2o3_pickled_tensor):
+        for file_name in fe2o3_pickled_tensor.keys():
+            assert fe2o3_pickled_tensor[file_name].is_consistent(fe2o3_pickled_tensor[file_name])
+
+    def test_tensors_inter_consistent(self, fe2o3_pickled_tensor):
+        for file_name in fe2o3_pickled_tensor.keys():
+            for other_file in fe2o3_pickled_tensor.keys():
+                if file_name != other_file:
+                    assert fe2o3_pickled_tensor[file_name].is_consistent(fe2o3_pickled_tensor[other_file])
+
+    def test_expected_attribs(self, fe2o3_pickled_tensor):
+        for tensor in fe2o3_pickled_tensor.values():
+            assert tensor.n_beams == 38
+            assert tensor.n_energies == 208
+            assert len(tensor.e_kin) == 208
