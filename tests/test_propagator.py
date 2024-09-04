22638b6358c0b0de94fd466af7a6486a469a5870
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-09-04 14:19:23 +0200

    test,feat: tests for propagator values

---
+++ tests/test_propagator.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+import pytest
+import numpy as np
+import jax
+
+import pytest
+
+from viperleed_jax.propagator import calc_propagator
+from viperleed_jax.constants import BOHR
+from viperleed_jax.lib_math import EPS
+
+
+@pytest.fixture(scope='session')
+def stored_propagator_reference_values():
+    file = Path(__file__).parent / 'test_data' / 'reference_values' / 'propagator_reference_values.npz'
+    return np.load(file)['propagator_reference_values_l_max_18_e_1e0_v_imag_1e0'], 18, 1.0, 1.0
+
+JIT_CALC_PROPAGATOR = jax.jit(calc_propagator, static_argnums=(0,))
+
+TEST_DISP_VECTORS = (
+    np.array([0.1, 0.0, 0.0]),
+    np.array([0.0, 0.1, 0.0]),
+    np.array([0.0, 0.0, 0.1]),
+    np.array([0.1, 0.1, 0.0]),
+    np.array([0.1, 0.0, 0.1]),
+    np.array([0.1, 0.1, 0.1]),
+    np.array([-0.1, 0.0, 0.0]),
+    np.array([-0.1, -0.1, -0.]),
+    np.array([0.0, -0.1, 0.1]),
+    np.array([1.0, 0.0, 0.0]),
+    np.array([1.0, 2.0, 3.0]),
+    np.array([1e-4, 1e-4, 1e-4]),
+)
+
+class TestPropagator:
+    # Propagators for a vanishing displacement should always yield the
+    # identity matrix
+    @pytest.mark.parametrize("l_max", range(5, 18))
+    def test_propagator_zero_displacement(self, l_max):
+        disp_vector = np.array([0.0, 0.0, 0.0])
+        propagator = calc_propagator(l_max, disp_vector, 1.0, 1.0)
+        assert propagator == pytest.approx(np.identity((l_max+1)**2), abs=1e-8)
+
+
+    @pytest.mark.parametrize("disp_vector", list(enumerate(TEST_DISP_VECTORS)))
+    def test_propagator_with_displacement(self, disp_vector, stored_propagator_reference_values):
+        """Check that the propagator gives the expected result."""
+        n_vector, disp_vector = disp_vector
+        stored_propagators, l_max, energy, v_imag = stored_propagator_reference_values
+        reference_value = stored_propagators[n_vector]
+        # calculate the propagator
+        propagator = calc_propagator(l_max, disp_vector, energy, v_imag)
+        assert propagator == pytest.approx(reference_value, rel=1e-6, abs=1e-8)
+
+    @pytest.mark.parametrize("disp_vector", list(enumerate(TEST_DISP_VECTORS)))
+    def test_propagator_jit(self, disp_vector, stored_propagator_reference_values):
+        """Check if the jit compiled function gives the same result."""
+        n_vector, disp_vector = disp_vector
+        stored_propagators, l_max, energy, v_imag = stored_propagator_reference_values
+        reference_value = stored_propagators[n_vector]
+        # jit compile
+
+        # calculate the propagator
+        propagator = JIT_CALC_PROPAGATOR(l_max, disp_vector, energy, v_imag)
+        assert propagator == pytest.approx(reference_value, rel=1e-6, abs=1e-8)
