5eeb78e6b99c52c1e4c08bc83969fc261a4d7bb6
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-09-05 09:42:13 +0200

    test,style: use constant

--- tests/test_atomic_units.py
+++ tests/test_atomic_units.py
@@ -1,7 +1,7 @@
 import pytest
 import jax.numpy as jnp
 
-from viperleed_jax.constants import BOHR
+from viperleed_jax.constants import BOHR, HARTREE
 from viperleed_jax import atomic_units
 
 @pytest.mark.parametrize("displacement_vector_ang, expected_vector", [
@@ -14,10 +14,10 @@ def test_to_internal_displacement_vector(displacement_vector_ang, expected_vecto
     assert result == pytest.approx(expected_vector)
 
 @pytest.mark.parametrize("energy_eV, expected_au", [
-    (27.21138602, 1.0),  # 1 Hartree in eV is exactly 1 atomic unit of energy
-    (54.42277204, 2.0),  # 2 Hartrees in eV
+    (1*HARTREE, 1.0),  # 1 Hartree in eV is exactly 1 atomic unit of energy
+    (2*HARTREE, 2.0),  # 2 Hartrees in eV
     (0.0, 0.0),          # 0 eV should be 0 atomic units
-    (13.60569301, 0.5),  # 0.5 Hartree in eV
+    (0.5*HARTREE, 0.5),  # 0.5 Hartree in eV
 ])
 def test_to_atomic_unit_energy(energy_eV, expected_au):
     assert atomic_units.to_atomic_unit_energy(energy_eV) == pytest.approx(expected_au)
