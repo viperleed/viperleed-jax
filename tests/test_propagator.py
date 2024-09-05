81f3ce440ff5203ef0827ba8130adef96ed70a24
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-09-05 14:00:49 +0200

    test: propagtor rotation  symmetry again

--- tests/test_propagator.py
+++ tests/test_propagator.py
@@ -5,7 +5,7 @@ import jax
 
 import pytest
 
-from viperleed_jax.propagator import calc_propagator
+from viperleed_jax.propagator import calc_propagator, symmetry_tensor
 from viperleed_jax.constants import BOHR
 from viperleed_jax.lib_math import EPS
 
@@ -178,3 +178,62 @@ class TestPropagator:
         assert propagator_jac == pytest.approx(
             reference_value, rel=5e-5, abs=1e-7
         )
+
+def rot_matrix(theta):
+    """Return a 2D rotation matrix."""
+    return np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)]
+    ])
+
+TEST_PLANE_SYMMETRY_OPERATIONS = {
+    # identity
+    'identity': np.identity(2),
+    # rotations
+    'pi/6': rot_matrix(np.pi/6),
+    'pi/4': rot_matrix(np.pi/4),
+    'pi/2': rot_matrix(np.pi/2),
+    'pi': rot_matrix(np.pi),
+    '0.123': rot_matrix(0.123),
+    # mirror operations
+}
+
+class TestSymmetryTensor:
+    L_MAX = 8
+    ENERGY = 1.0
+    V_IMAG = 1.0
+
+    def test_symmetry_tensor_identity(self):
+        """Check that the symmetry tensor for the identity is ones."""
+        assert symmetry_tensor(self.L_MAX, np.identity(2)) == pytest.approx(
+            np.ones(((self.L_MAX + 1) ** 2, (self.L_MAX + 1) ** 2)), abs=1e-8
+        )
+
+    @pytest.mark.parametrize("plane_symmetry_operation",
+                             TEST_PLANE_SYMMETRY_OPERATIONS.values(),
+                             ids=TEST_PLANE_SYMMETRY_OPERATIONS.keys())
+    @pytest.mark.parametrize("disp_vector", TEST_DISP_VECTORS)
+    def test_symmetry_tensor(self, plane_symmetry_operation, disp_vector):
+
+        # add z to get full 3d symmetry operation
+        # NB: coordinates are stored as (z, x, y)!
+        rotation_matrix_3d = np.identity(3)
+        rotation_matrix_3d[1:3, 1:3] = plane_symmetry_operation
+
+        # apply the symmetry operation to the displacement vector
+        disp_vector_sym = rotation_matrix_3d @ disp_vector
+
+        # calculate the propagator for both
+        propagator_original = calc_propagator(self.L_MAX, disp_vector,
+                                              self.ENERGY, self.V_IMAG)
+        propagator_sym = calc_propagator(self.L_MAX, disp_vector_sym,
+                                         self.ENERGY, self.V_IMAG)
+
+        # apply the symmetry operation to the propagator
+        # (element-wise multiplication)
+        propagator_original_sym = propagator_original * symmetry_tensor(self.L_MAX,
+                                                                       plane_symmetry_operation)
+
+        # check if the propagator is the same
+        assert propagator_sym == pytest.approx(propagator_original_sym,
+                                               abs=1e-7)
