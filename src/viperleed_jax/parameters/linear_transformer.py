6b1c683f36a3aba4cc29e0223ed0f2f36088aff3
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-10-08 09:59:21 +0200

    feat: update linear transformer

--- src/viperleed_jax/parameters/linear_transformer.py
+++ src/viperleed_jax/parameters/linear_transformer.py
@@ -10,18 +10,59 @@ class LinearTransformer:
         self.biases = jnp.array(biases)
         self.out_reshape = out_reshape
 
+        self._in_dim = self.n_free_params
+        self._out_dim = self.biases.shape[0]
+
+        # consistency check of dimensions
+        if self.weights.shape[0] != self._out_dim:
+            raise ValueError(
+                f"Weight matrix shape {self.weights.shape} does not match "
+                f"bias shape {self.biases.shape}"
+            )
+        if self.out_reshape is not None:
+            if self._out_dim != jnp.prod(self.out_reshape):
+                raise ValueError(
+                    f"Output reshape {self.out_reshape} does not match bias "
+                    f"shape {self.biases.shape}"
+                )
+
+    @property
+    def in_dim(self):
+        return self._in_dim
+
+    @property
+    def out_dim(self):
+        return self._out_dim
+
     def __call__(self, free_params):
         if self.n_free_params == 0:
             return self.biases
-        if isinstance(free_params, float):
-            free_params = jnp.array([free_params])
-        free_params = jnp.array(free_params)
+        free_params = jnp.asarray(free_params)
         if len(free_params) != self.n_free_params:
             raise ValueError("Free parameters have wrong shape")
-        result = self.weights @ free_params + self.biases
+        result = self.weights @ free_params + self.biases  # Ax + b
         if self.out_reshape is not None:
             result = result.reshape(self.out_reshape)
         return result
 
     def __repr__(self):
         return f"LinearTransformer(weights={self.weights.shape}, biases={self.biases.shape}, out_reshape={self.out_reshape})"
+
+    def tree_flatten(self):
+        aux_data = {
+            "n_free_params": self.n_free_params,
+            "weights": self.weights,
+            "biases": self.biases,
+            "out_reshape": self.out_reshape,
+            "_in_dim": self._in_dim,
+            "_out_dim": self._out_dim,
+        }
+        children = None
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, children, aux_data):
+        frozen_parameter_space = cls.__new__
+        for kw, value in aux_data.items():
+            setattr(frozen_parameter_space, kw, value)
+        return frozen_parameter_space
