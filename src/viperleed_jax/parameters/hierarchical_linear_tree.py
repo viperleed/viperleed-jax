e3524d9071c71676265a2414119a9527424a2a5b
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-10-09 09:31:31 +0200

    feat: ParameterHLSubtree draft

--- src/viperleed_jax/parameters/hierarchical_linear_tree.py
+++ src/viperleed_jax/parameters/hierarchical_linear_tree.py
@@ -1,3 +1,5 @@
+from collections.abc import ABC, abstractmethod
+
 import numpy as np
 import jax.numpy as jnp
 import anytree
@@ -114,6 +116,61 @@ class HLConstraintNode(HLNode):
         # if that went well, set the children
         for child in _children:
             child.parent = self
+
+
+
+class ParameterHLSubtree(ABC):
+    """Base class representing a subtree in the hierarchical linear tree.
+
+    Subtrees are used to group nodes for a group of parameters (vib, geo, occ,
+    V0r). This allows implementing constraints more easily and makes it possible
+    to extract additional information from the tree (e.g. propagator
+    transformations).
+    """
+
+    def __init__(self, slab, atom_site_elements, site_elements):
+        self.slab = slab
+        self.atom_site_elements = atom_site_elements
+        self.site_elements = site_elements
+        self.nodes = nodes
+        self.name = name
+        self._subtree_root_has_been_created = False
+
+    @property
+    def roots(self):
+        return [node for node in self.nodes if node.is_root]
+
+    @property
+    @abstractmethod
+    def subtree_root_name(self):
+        pass
+
+    @abstractmethod
+    def build_subtree(self):
+        """Method to build the subtree for the parameter group."""
+        pass
+
+
+    def create_subtree_root(self):
+        """Create a root node that aggregates all root nodes in the subtree.y"""
+        if self._subtree_root_has_been_created:
+            raise ValueError("Subtree root has already been created.")
+        self._subtree_root_has_been_created = True
+        if not self.roots:
+            raise ValueError("No root nodes found in subtree.")
+        root_dof = sum(node.dof for node in self.roots)
+        transformers = []
+        for node in self.roots:
+            weights = np.zeros((node.dof, root_dof))
+            weights[:, : node.dof] = np.identity(node.dof)
+            bias = np.zeros(node.dof)
+            transformers.append(LinearTransformer(weights, bias, (node.dof,)))
+        return HLConstraintNode(
+            dof=root_dof,
+            name=self.subtree_root_name,
+            children=self.roots,
+            transformers=transformers,
+    )
 
 
 def create_subtree_root(subtree_nodes, name):
