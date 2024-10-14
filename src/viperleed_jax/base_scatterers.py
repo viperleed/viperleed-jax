b9fc7f80839e69fb98bff75f6eee6cb1eddc04c6
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-10-14 16:23:08 +0200

    style: module header

--- src/viperleed_jax/base_scatterers.py
+++ src/viperleed_jax/base_scatterers.py
@@ -1,3 +1,8 @@
+"""Module base_scatterers of viperleed_jax."""
+
+__authors__ = ("Alexander M. Imre (@amimre)",)
+__created__ = "2024-10-14"
+
 from collections import namedtuple
 
 SiteEl = namedtuple("SiteEl", ["site", "element"])
