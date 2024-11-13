3f143d7e1c134d6e9012f5610de3babfead7a922
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-11-13 11:35:41 +0100

    fix: numpy error

--- src/viperleed_jax/parameters/hierarchical_linear_tree.py
+++ src/viperleed_jax/parameters/hierarchical_linear_tree.py
@@ -530,7 +530,7 @@ class HLSubtree(ABC):
                 dummy_transformer.in_dim, dtype=bool, fill_value=True
             )
             dummy_arr = np.asarray(dummy_transformer(input))
-            is_dynamic.append(dummy_arr)
+            is_dynamic.append(np.any(dummy_arr))
         return np.array(is_dynamic, dtype=bool)
 
 
