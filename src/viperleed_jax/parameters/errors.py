ffca25f6d955a984785f7a7d6b8dd8b3681539be
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-11-25 12:27:05 +0100

    feat: add errors file

---
+++ src/viperleed_jax/parameters/errors.py
@@ -0,0 +1,8 @@
+"""Module transformation_tree/errors."""
+
+__authors__ = ('Alexander M. Imre (@amimre)',)
+__created__ = '2024-11-25'
+
+
+class TransformationTreeError(Exception):
+    """Base class for errors in the transformation tree and nodes."""
