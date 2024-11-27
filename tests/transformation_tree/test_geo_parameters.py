28a126d7a2fdea126e64b2f9ef0690087169f17b
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2024-11-27 11:10:16 +0100

    test: symmetry op determinants

---
+++ tests/transformation_tree/test_geo_parameters.py
@@ -0,0 +1,27 @@
+import pytest
+import numpy as np
+from pytest_cases import parametrize_with_cases
+
+from viperleed_jax.transformation_tree.displacement_tree_layers import (
+    DisplacementTreeLayers,
+)
+from viperleed_jax.base_scatterers import BaseScatterers
+from viperleed_jax.transformation_tree.geo_parameters import GeoTree
+
+from ..structures import CaseStatesAfterInit, Tag
+
+
+
+@parametrize_with_cases('test_case', cases=CaseStatesAfterInit)
+def test_symmetry_operations_determinant(test_case):
+    """The abs of the determinant of symmetry operations should be 1."""
+    state, _ = test_case
+    base_scatterers = BaseScatterers(state.slab)
+    # create the geometry tree
+    tree = GeoTree(base_scatterers)
+
+    sym_ops = [
+        leaf.symmetry_operation_to_reference_propagator for leaf in tree.leaves
+    ]
+    sym_op_dets = np.array([np.linalg.det(sym_op) for sym_op in sym_ops])
+    assert abs(sym_op_dets) == pytest.approx(1.0)
