d389e52bb6d8b624030efcd72a32d4a927ab98f9
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-15 17:10:16 +0200

    refactor: move selection logic to atom basis

--- src/viperleed_jax/atom_basis.py
+++ src/viperleed_jax/atom_basis.py
@@ -5,10 +5,22 @@ __created__ = '2024-10-14'
 
 from collections import namedtuple
 
+import numpy as np
+
+from viperleed_jax.files.displacements.tokens.target import TargetToken
+
 SiteEl = namedtuple('SiteEl', ['site', 'element'])
+
+# Note: The naming of these clasess may still need to be discussed.
+# They are different from the atom class in base viperleed calc. It may make
+# sense to merge the AtomBasis into viperleed.calc's Slab class.
+
 
 # Currently, the internal numbering is a bit inconsitent.
 # Atom numbers start at 1, but layer numbers start at 0.
+
+class TargetSelectionError(ValueError):
+    """Error raised if target selection goes wrong."""
 
 
 class Atom:
@@ -120,3 +132,62 @@ class AtomBasis:
                 if siteel.site == at.site.label:
                     atom_basis.append(Atom(at, siteel))
         return tuple(atom_basis)  # read only from here on out
+
+
+    def _selection_mask_from_target_token(self, target_token):
+        """Select base scatterers that match the target specification."""
+        mask = np.full(len(self), fill_value=True)
+
+        # mask based on the site
+        matches = np.array(
+            [target_token.regex.match(bs.site) is not None for bs in self]
+        )
+        mask = mask & matches
+
+        # mask based on the labels
+        label_mask = mask.copy()
+
+        # If nums are specified, apply the selection based on nums
+        if target_token.nums is not None:
+            # check range for nums
+            if any(num < 1 or num > len(self) for num in target_token.nums):
+                msg = f'Invalid atom number for target: {target_token.target_str}'
+                raise TargetSelectionError(msg)
+            num_mask = np.array([bs.num in target_token.nums for bs in self])
+            # check if any of the given nums have the wrong label
+            wrong_label = np.logical_and(num_mask, ~label_mask)
+            if np.any(wrong_label):
+                msg = (
+                    'Atom numbers do not match label for target: '
+                    f'{target_token.target_str}'
+                )
+                raise TargetSelectionError(msg)
+            mask = mask & num_mask
+
+        # If layers are specified, apply the selection based on layers
+        if target_token.layers is not None:
+            mask = mask & np.array(
+                # TODO: layer counting from 1; can we unify this somewhere?
+                [bs.layer + 1 in target_token.layers for bs in self]
+            )
+
+        if mask.sum() == 0:
+            msg = f'No atoms selected for TargetToken: {target_token}'
+            raise TargetSelectionError(msg)
+
+        return mask
+
+
+    def selection_mask(self, targets):
+        """Take the 'or' of all targets, combining masks."""
+        if not all(isinstance(target, TargetToken) for target in targets):
+            raise TargetSelectionError(
+                'All supplied targets must be of TargetToken type.'
+            )
+
+        combined_mask = np.full(len(self), fill_value=False)
+        for target in targets:
+            combined_mask = (
+                combined_mask | self._selection_mask_from_target_token(target)
+            )
+        return combined_mask
