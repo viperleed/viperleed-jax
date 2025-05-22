864486a09e8d49dacbd0b643e071cf4578f34407
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-22 16:25:27 +0200

    test,fix,feat: use non mock nodes

--- tests/transformation_tree/test_nodes.py
+++ tests/transformation_tree/test_nodes.py
@@ -48,29 +48,28 @@ def dummy_leaf_node():
     """Fixture for a dummy leaf node."""
     return LinearLeafNode(dof=3, name='dummy_leaf')
 
-def test_transformation_tree_node():
+def test_transformation_tree_node(dummy_leaf_node):
     """Test basic functionality of TransformationTreeNode."""
-    node = MockTransformationTreeNode(name='root', parent=None)
-    assert node._name == 'root'
+    assert dummy_leaf_node._name == 'dummy_leaf'
     with pytest.raises(InvalidNodeError):
-        _ = node.transformer  # Transformer is not set
+        _ = dummy_leaf_node.transformer  # Transformer is not set
 
-    transformer = AffineTransformer(weights=np.eye(2), biases=np.zeros(2))
-    node.set_transformer(transformer)
-    assert node.transformer is transformer
+    transformer = AffineTransformer(weights=np.eye(3), biases=np.zeros(3))
+    dummy_leaf_node.set_transformer(transformer)
+    assert dummy_leaf_node.transformer is transformer
 
 
 def test_type_error_set_transformer(dummy_leaf_node):
     """Test basic functionality of TransformationTreeNode."""
 
-    not_a_transformer = np.eye(2)
+    not_a_transformer = np.eye(3)
     with pytest.raises(TypeError):
         dummy_leaf_node.set_transformer(not_a_transformer)
 
 
 def test_linear_tree_node_transformer_validation():
     """Test transformer validation in LinearTreeNode."""
-    node = MockLinearTreeNode(dof=2, layer=DisplacementTreeLayers.Base)
+    node = LinearLeafNode(dof=2, name='leaf')
     valid_transformer = AffineTransformer(weights=np.eye(2), biases=np.zeros(2))
     invalid_transformer = AffineTransformer(
         weights=np.eye(3), biases=np.zeros(3)
