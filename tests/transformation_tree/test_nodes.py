b02ae2ea62ec45dc12f6182a42ddb7820785b85d
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-05-22 16:38:12 +0200

    test: remove mock classes, test linking

--- tests/transformation_tree/test_nodes.py
+++ tests/transformation_tree/test_nodes.py
@@ -16,31 +16,14 @@ from viperleed_jax.transformation_tree.errors import (
 )
 from viperleed_jax.transformation_tree.linear_transformer import (
     AffineTransformer,
+    LinearMap
 )
 from viperleed_jax.transformation_tree.nodes import (
     LinearLeafNode,
     LinearTreeNode,
+    LinearConstraintNode,
     TransformationTreeNode,
 )
-
-
-class MockTransformationTreeNode(TransformationTreeNode):
-    def set_transformer(self, transformer):
-        self._transformer = transformer
-
-
-class MockLinearTreeNode(LinearTreeNode):
-    def __init__(self, dof, layer, name=None, parent=None, children=None):
-        super().__init__(
-            dof, layer, name=name, parent=parent, children=children
-        )
-
-    def set_transformer(self, transformer):
-        if not isinstance(transformer, AffineTransformer):
-            raise TypeError('Invalid transformer type.')
-        if transformer.out_dim != self.dof:
-            raise ValueError('Transformer output dimension mismatch.')
-        self._transformer = transformer
 
 
 @pytest.fixture
@@ -61,13 +44,41 @@ def test_transformation_tree_node(dummy_leaf_node):
 
 def test_type_error_set_transformer(dummy_leaf_node):
     """Test basic functionality of TransformationTreeNode."""
-
     not_a_transformer = np.eye(3)
     with pytest.raises(TypeError):
         dummy_leaf_node.set_transformer(not_a_transformer)
 
+def test_detaching_parent_not_allowed():
+    """Test that detaching parents in TransformationTreeNode is not allowed."""
+    # Create a mock node
+    node = TransformationTreeNode(name='child', parent=None)
+    with pytest.raises(
+        TransformationTreeError,
+        match='Transformation trees do not support detaching nodes.',
+    ):
+        node._pre_detach()
 
-def test_linear_tree_node_transformer_validation():
+
+# Test the name property of LinearTreeNode
+def test_linear_tree_node_name_property():
+    node = LinearLeafNode(dof=3, name='foo')
+    assert node._name == 'foo'
+    assert node.name == '(3) foo'
+
+class TestLinearLeafNode:
+    # Test LinearLeafNode properties and transformer setting
+    def test_linear_leaf_node_properties_and_transformer(self):
+
+        node = LinearLeafNode(dof=2, name='leaf')
+        assert node.dof == 2
+        assert node.name == '(2) leaf'
+
+        # test transformer setting
+        valid_transformer = AffineTransformer(weights=np.eye(2), biases=np.zeros(2))
+        node.set_transformer(valid_transformer)
+        assert node.transformer == valid_transformer
+
+def test_transformer_validation():
     """Test transformer validation in LinearTreeNode."""
     node = LinearLeafNode(dof=2, name='leaf')
     valid_transformer = AffineTransformer(weights=np.eye(2), biases=np.zeros(2))
@@ -84,41 +95,30 @@ def test_linear_tree_node_transformer_validation():
         )  # Mismatched transformer output dimension
 
 
-def test_detaching_parent_not_allowed():
-    """Test that detaching parents in TransformationTreeNode is not allowed."""
-    # Create a mock node
-    node = MockTransformationTreeNode(name='child', parent=None)
-    with pytest.raises(
-        TransformationTreeError,
-        match='Transformation trees do not support detaching nodes.',
-    ):
-        node._pre_detach()
-
-
-# Test the name property of LinearTreeNode
-def test_linear_tree_node_name_property():
-    node = MockLinearTreeNode(
-        dof=3, layer=DisplacementTreeLayers.Base, name='foo')
-    assert node._name == 'foo'
-    assert node.name == '(3) foo'
-
-
-# Test LinearLeafNode properties and transformer setting
-def test_linear_leaf_node_properties_and_transformer():
-
-    node = LinearLeafNode(dof=2, name='leaf')
-    assert node.dof == 2
-    assert node.name == '(2) leaf'
-
-    # test transformer setting
-    valid_transformer = AffineTransformer(weights=np.eye(2), biases=np.zeros(2))
-    node.set_transformer(valid_transformer)
-    assert node.transformer == valid_transformer
-
-
+class TestTreeLinking:
 # Test parent-child relationship in tree nodes
-def test_parent_child_relationship_in_tree_nodes():
-    parent = MockTransformationTreeNode(name='parent', parent=None)
-    child = MockTransformationTreeNode(name='child', parent=parent)
-    assert child.parent is parent
-    assert child in parent.children
+    def test_parent_child_relationship_in_tree_nodes(self):
+        grandparent = TransformationTreeNode(name='grandparent', parent=None)
+        parent = TransformationTreeNode(name='parent', parent=grandparent)
+        child = TransformationTreeNode(name='child', parent=parent)
+        assert child.parent is parent
+        assert child in parent.children
+        assert parent.parent is grandparent
+        assert child in grandparent.descendants
+        assert grandparent in child.ancestors
+
+    def test_parent_child_relationship_in_linear_tree_nodes(
+        self, dummy_leaf_node):
+        child = dummy_leaf_node
+        direct_map = LinearMap(np.eye(3))
+        parent = LinearConstraintNode(
+            dof=3, layer=DisplacementTreeLayers.Symmetry,
+            name='sym_parent',
+            transformers=[direct_map],
+            children=[child]
+        )
+
+        # test transformer to descendent
+        assert parent.transformer_to_descendent(child) == direct_map
+
+# Test that a node cannot be added to multiple parents
