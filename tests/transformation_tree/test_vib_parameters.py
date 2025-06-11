c18250293e8c55a447b0abd5b34ff27458b1da82
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-06-11 13:56:53 +0200

    test: tree creation tests vib params

--- tests/transformation_tree/test_vib_parameters.py
+++ tests/transformation_tree/test_vib_parameters.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pytest
 from pytest_cases import fixture, parametrize_with_cases
-
-from viperleed_jax.atom_basis import AtomBasis
 from viperleed.calc.files.new_displacements.lines import (
     ConstraintLine,
     OffsetsLine,
     VibDeltaLine,
 )
+
+from viperleed_jax.atom_basis import AtomBasis
 from viperleed_jax.transformation_tree.displacement_tree_layers import (
     DisplacementTreeLayers,
 )
@@ -20,6 +20,20 @@ from viperleed_jax.transformation_tree.reduced_space import (
 from viperleed_jax.transformation_tree.vib_parameters import VibTree
 
 from ..structures import CaseStatesAfterInit
+
+
+def test_tree_creation(atom_basis, subtests):
+    """Test tree creation."""
+    # create the geometry tree
+    tree = VibTree(atom_basis)
+    assert len(tree.leaves) == len(atom_basis)
+
+    with subtests.test('tree root creation'):
+        # apply implicit constraints to unmodified tree
+        tree.apply_implicit_constraints()
+        tree.finalize_tree()
+        assert tree.root.is_root
+        assert tree.root.is_leaf is False
 
 
 class TestFe2O3:
@@ -40,7 +54,7 @@ class TestFe2O3:
         last_node = fe2o3_tree.nodes[-1]
         assert len(last_node.children) == 1
         assert last_node.children[0].transformer.biases == pytest.approx(
-            np.array(0.1)
+            np.array([0.1])
         )
         assert last_node.children[0].transformer.weights == pytest.approx(
             np.eye(1)
