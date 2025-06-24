a743945fc0c9f19591418b948a043edf1cf38d6f
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-06-24 10:53:27 +0200

    feat: WIP derived quantities

---
+++ src/viperleed_jax/lib/derived_quantities/base.py
@@ -0,0 +1,43 @@
+
+from abc import ABC, abstractmethod
+
+from ..calculator import normalize_occ_vector
+from ...atomic_units import to_internal_displacement_vector
+
+class DerivedQuantity(ABC):
+    def __init__(self, tree):
+        self.tree = tree
+
+    @abstractmethod
+    def __call__(self, params):
+        pass
+
+
+
+class NormalizedOccupations(DerivedQuantity):
+    """Derived quantity for normalized occupations."""
+
+    def __init__(self, tree, atom_ids):
+        super().__init__(tree)
+        self.name = 'normalized_occupations'
+        self.atom_ids = tuple(atom_ids)
+
+    def __call__(self, params):
+        """Calculate normalized occupations."""
+        non_normalized_occupations = self.tree(params)
+        return normalize_occ_vector(
+            non_normalized_occupations,
+            self.atom_ids
+        )
+
+class AtomicUnitsDisplacements(DerivedQuantity):
+    """Derived quantity for atomic units displacements."""
+
+    def __init__(self, tree):
+        super().__init__(tree)
+        self.name = 'atomic_units_displacements'
+
+    def __call__(self, params):
+        """Calculate displacements in atomic units."""
+        displacements = self.tree(params)
+        return to_internal_displacement_vector(displacements)
