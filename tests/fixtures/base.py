c0eec8759708ffb7ad29a3d1265b965b580ba31c
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-06-27 10:42:21 +0200

    feat,test: add large file tracking via git LFS

--- tests/fixtures/base.py
+++ tests/fixtures/base.py
@@ -7,7 +7,7 @@ from pathlib import Path
 if 'VIPERLEED_ON_THE_FLY_TESTS_LARGE_FILE_PATH' not in os.environ:
     raise ValueError('VIPERLEED_ON_THE_FLY_TESTS_LARGE_FILE_PATH not set')
 
-LARGE_FILE_PATH = Path(os.environ['VIPERLEED_ON_THE_FLY_TESTS_LARGE_FILE_PATH'])
+LARGE_FILE_PATH = Path(__file__).parent.parent / 'test_data' / 'large_files'
 
 if not LARGE_FILE_PATH.exists():
     raise ValueError(f'Large file path {LARGE_FILE_PATH} does not exist')
