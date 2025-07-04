56c2b24dfe9de9c140aec8ad0dce6c3f79c770af
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-07-04 14:44:20 +0200

    fix: KeyError

--- src/viperleed_jax/from_objects.py
+++ src/viperleed_jax/from_objects.py
@@ -146,9 +146,9 @@ def setup_tl_calculator(
 
 
 def _determine_l_max(rpars):
-    if rpars.VLJ_CONFIG['t_leed_l_max'] == -1:
+    if rpars.VLJ_CONFIG['t-leed-l_max'] == -1:
         # use the maximum l_max from the reference calculation
         return rpars.LMAX.max
 
     # use the l_max from the rparams
-    return rpars.VLJ_CONFIG['t_leed_l_max']
+    return rpars.VLJ_CONFIG['t-leed-l_max']
