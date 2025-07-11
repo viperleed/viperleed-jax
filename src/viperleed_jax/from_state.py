f4b93e4c4ff94f00a0cb167ded2fce7fc5460699
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-07-11 17:16:50 +0200

    fix: from state with jax backend

--- src/viperleed_jax/from_state.py
+++ src/viperleed_jax/from_state.py
@@ -99,7 +99,8 @@ def run_viperleed_initialization(calc_path):
         os.chdir(tmp_calc_path)
         try:
             exit_code, state_recorder = run_calc(
-                'tmp_init', preset_params={'RUN': [0]}
+                'tmp_init', preset_params={
+                    'RUN': [0],'BACKEND': {'search': 'viperleed-jax'}}
             )
         finally:  # always change back to the original directory
             os.chdir(home)
