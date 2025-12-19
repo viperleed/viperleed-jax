47e24acc0d7dbc262c60ee36ca3aa3acc169279e
Alexander M. Imre <90624999+amimre@users.noreply.github.com>
2025-12-19 11:57:43 +0100

    chore,refactor: move files and classes

--- src/viperleed_jax/optimization/history.py
+++ src/viperleed_jax/optimization/history.py
@@ -4,12 +4,9 @@ __authors__ = ('Alexander M. Imre (@amimre)',)
 __created__ = '2025-03-11'
 
 
-import re
 import time
-from pathlib import Path
 
 import numpy as np
-from viperleed.calc.constants import DEFAULT_OUT
 
 
 class OptimizationHistory:
@@ -321,25 +318,3 @@ class OptimizationHistory:
             f'Duration: {self.duration:.2f}s\n'
             f'Best R:   {self.best_R:.4f}'
         )
-
-
-class RefCalcHistory:
-    # TODO: implement functionality to read and store ref-calc duration and timestamp
-
-    def __init__(self, history_dir):
-        history_dir = Path(history_dir)
-        try:
-            self.ref_R = self._read_ref_rfactor(history_dir)
-        except ValueError as err:
-            msg = f'Unable to read R factor from directory {history_dir}.'
-            raise ValueError(msg) from err
-
-    @staticmethod
-    def _read_ref_rfactor(history_dir):
-        out_dir = history_dir / DEFAULT_OUT
-        file = list(out_dir.glob('R_OUT_refcalc_*'))[0]
-        match = re.search(r'R=([0-9.]+)', file.name)
-        return float(match.group(1))
-
-    def __repr__(self):
-        return f'RefCalcHistory R: {self.ref_R}'
